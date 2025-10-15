4a44eba02f629f0047587f060ee9c501b1351567
chehalx <chehal.minocha@gmail.com>
2025-10-15 09:09:03 +0530

    Set SQLALCHEMY_DATABASE_URI from environment

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ app = Flask(__name__)
 app.secret_key = os.environ.get("SESSION_SECRET", "healthsense_default_secret")
 
 # Configure database
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
 app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
     "pool_recycle": 300,
     "pool_pre_ping": True,
