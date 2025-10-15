911bb77d41cf5e5654d19fdc869d86262bd89f50
chehalx <chehal.minocha@gmail.com>
2025-10-15 08:50:48 +0530

    Update main.py for production deployment

--- main.py
+++ main.py
@@ -6,3 +6,5 @@ from views import *  # noqa: F401
 # Run the app if executed directly
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port, debug=False)
